0ef0eae1c76151fc79b875f8c4646b856d10b52a
Snehal Mundhe <m.snehal106@gmail.com>
2021-01-01 18:57:47 -0500

    feat: added chat and sidebar components

--- juschat-client/src/Chat.js
+++ juschat-client/src/Chat.js
@@ -1,10 +1,41 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Chat.css'
+import { IconButton} from "@material-ui/core"
+import {AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons"
+
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am a Chat</h1>
+           <div className="chat__header">
+               <Avatar/>
+               <div className="chat__headerInfo">
+                   <h3>Room name</h3>
+                   <p>Last seen at .....</p>
+               </div>
+               <div className="chat__headerRight">
+               <IconButton>  
+                  <SearchOutlined/>
+                  </IconButton>  
+                  <IconButton>  
+                    <AttachFile/>
+                  </IconButton>
+                  <IconButton>  
+                    <MoreVert/>
+                  </IconButton>    
+               </div>
+           </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                  <span className="chat__name">Snehal</span>  
+                    This is the message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
         </div>
     )
 }
--- juschat-client/src/Sidebar.js
+++ juschat-client/src/Sidebar.js
@@ -4,13 +4,16 @@ import DonutLargeIcon from "@material-ui/icons/DonutLarge"
 import {Avatar, IconButton} from "@material-ui/core"
 import ChatIcon from "@material-ui/icons/Chat"
 import MoreVertIcon from "@material-ui/icons/MoreVert"
+import {SearchOutlined} from "@material-ui/icons"
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar 
-                src="https://avatars3.githubusercontent.com/u/49169934?s=400&u=56c4a06482743507101af3c9dcd809c247bda85f&v=4"/>
+                src="https://avatars3.githubusercontent.com/
+                u/49169934?s=400&u=56c4a06482743507101af3c9dcd809c247bda85f&v=4"/>
                 <div className="sidebar__headerRight">
                   <IconButton>  
                     <DonutLargeIcon/>
@@ -22,6 +25,19 @@ function Sidebar() {
                     <MoreVertIcon/>
                   </IconButton>    
                 </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start a new chat"
+                    type="text"/>
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+
             </div>
         </div>
     )
